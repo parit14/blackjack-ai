67478e59b0bb2b76b3800ce476a441e1ceeecb78
parit14 <toshiekaushik@gmail.com>
2024-07-06 17:37:48 -0700

    initial commit

---
+++ card_test.go
@@ -0,0 +1,77 @@
+package deck
+
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleCard() {
+	fmt.Println(Card{Rank: Ace, Suit: Heart})
+	fmt.Println(Card{Rank: Two, Suit: Spade})
+	fmt.Println(Card{Rank: Nine, Suit: Diamond})
+	fmt.Println(Card{Rank: Jack, Suit: Club})
+	fmt.Println(Card{Suit: Joker})
+
+	// OUtput:
+	// Ace of Hearts
+	// Two of Spades
+	// Nine of Diamonds
+	// Jack of Clubs
+	// Joker
+}
+
+func TestNew(t *testing.T) {
+	cards := New()
+	if len(cards) != 52 {
+		t.Error("Wrong number of cards in a new deck.")
+	}
+}
+
+func TestDefaultSort(t *testing.T) {
+	cards := New(DefaultSort)
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != exp {
+		t.Error("Expected Ace of Spades as first card. Received:", cards[0])
+	}
+}
+
+func TestSort(t *testing.T) {
+	cards := New(Sort(Less))
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != exp {
+		t.Error("Expected Ace of Spades as first card. Received:", cards[0])
+	}
+}
+
+func TestJokers(t *testing.T) {
+	cards := New(Jokers(3))
+	count := 0
+	for _, c := range cards {
+		if c.Suit == Joker {
+			count++
+		}
+	}
+	if count != 3 {
+		t.Error("Expected 3 Jokers, recieved: ", count)
+	}
+}
+
+func TestFilter(t *testing.T) {
+	filter := func(card Card) bool {
+		return card.Rank == Two || card.Rank == Three
+	}
+
+	cards := New(Filter(filter))
+	for _, c := range cards {
+		if c.Rank == Two || c.Rank == Three {
+			t.Error("Expected al twos and three to be filtered out.")
+		}
+	}
+}
+
+func TestDeck(t *testing.T) {
+	cards := New(Deck(3))
+	if len(cards) != 156 {
+		t.Errorf("Expected %d cards, received %d cards.", 156, len(cards))
+	}
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module deck
+
+go 1.22.5
+
+require (
+	golang.org/x/mod v0.18.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/tools v0.22.0 // indirect
+)
